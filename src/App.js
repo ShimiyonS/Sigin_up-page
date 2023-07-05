76ee9437b9f81b2ccc826ee0f8b37df59eb1260e
ShimiyonS <shimjnss@gmail.com>
2023-07-05 11:56:46 +0530

    hi

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
 import { useState } from "react";
 import "./signupPage.css";
 import axios from "axios";
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
   const [username, setUserName] = useState("");
   const [password, setPassword] = useState("");
   const [Phone, setPhone] = useState("");
   const [email, setEmail] = useState("");
-  const [success, setSuccess] = useState("");
-  const [confirmpassword, setConfirmPassword]= useState('');
-  const [error,setError]=useState("");
+  const [confirmpassword, setConfirmPassword] = useState("");
+
   const url = "https://surveyor-app-server.vercel.app/users/";
   const headers = {
     "Content-Type": "application/json",
   };
-  const getPostsData = (user) => {
-    axios
-      .post(url, user, { headers: headers })
-      .then((data) => setSuccess(data.data.message))
-      .catch((error) => setError(error));
-  };
+  // const getPostsData = (user) => {
+  //   axios
+  //     .post(url, user, { headers: headers })
+  //     .then((data) => toast.success(data.data.message))
+  //     .catch((error) => toast.error(error.response.data.message));
+  // };
   const submitHandler = (e) => {
     e.preventDefault();
-    if(password !== confirmpassword){
-      toast.error("Password Don't Match")
-    }else{
+    if (password !== confirmpassword) {
+      toast.error("Password Don't Match");
+    } else {
       const user = {
         userName: username,
         password: password,
@@ -32,11 +33,11 @@ function App() {
         number: Phone,
       };
       console.log(user);
-      getPostsData(user);
-      setTimeout(() => {
-        toast.success(success)
-        toast.error(error)
-        }, 600);
+      axios
+        .post(url, user, { headers: headers })
+        .then((data) => console.log(data))
+        .catch((error) => console.log(error));
+      //getPostsData(user);
     }
   };
   return (
@@ -52,31 +53,32 @@ function App() {
               name="Name"
               className="field"
               placeholder="Username"
-              required
               onChange={(e) => {
                 setUserName(e.target.value);
               }}
+              required
             />
             <label>password</label>
             <input
               type="password"
               name="password"
               placeholder="Password"
-              required
+              autoComplete="off"
               onChange={(e) => {
                 setPassword(e.target.value);
               }}
+              required
             />
             <label>confirm password</label>
             <input
               type="password"
               name="password"
+              autoComplete="off"
               onChange={(e) => {
                 setConfirmPassword(e.target.value);
               }}
               placeholder="Confirm password"
               required
-              
             />
             <label>phonenumber</label>
             <input
@@ -86,26 +88,25 @@ function App() {
               pattern="[0-9]*"
               maxLength="10"
               placeholder="Mobile No"
-              required
               onChange={(e) => {
                 setPhone(e.target.value);
               }}
+              required
             />
             <label>Email</label>
             <input
+              id="email"
               type="email"
               name="email"
-              pattern="/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/"
               placeholder="Email id"
-              required
               onChange={(e) => {
                 setEmail(e.target.value);
               }}
+              required
             />
             <br />
             <br />
             <button
-              type="submit"
               onClick={(e) => {
                 submitHandler(e);
               }}
@@ -113,10 +114,9 @@ function App() {
               Sign up
             </button>
           </form>
-         
         </div>
       </div>
-          <ToastContainer/>
+      <ToastContainer />
     </>
   );
 }
